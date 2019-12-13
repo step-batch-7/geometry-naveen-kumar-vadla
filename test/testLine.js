277720b91c9f2cb59c150a4e858982d75bc538f2
Vadla Naveen Kumar <58026402+naveenkumarvadla@users.noreply.github.com>
2019-12-13 22:24:57 +0530

    Added some more test cases for slope

--- test/testLine.js
+++ test/testLine.js
@@ -43,10 +43,12 @@ describe("Line", () => {
 			const line = new Line({ x: 1, y: 2 }, { x: 3, y: 4 });
 			assert.approximately(line.length, 2.8, 0.2);
 		});
+
 		it("Should give length of the given line having negative points", () => {
 			const line = new Line({ x: 1, y: -2 }, { x: -3, y: 4 });
 			assert.approximately(line.length, 7.2, 0.2);
 		});
+
 		it("Should give length of the given line having same points", () => {
 			const line = new Line({ x: 1, y: 2 }, { x: 1, y: 2 });
 			assert.strictEqual(line.length, 0);
@@ -58,17 +60,35 @@ describe("Line", () => {
 			const line = new Line({ x: 1, y: 2 }, { x: 3, y: 4 });
 			assert.strictEqual(line.slope, 1);
 		});
+
 		it("Should give slope of given line having negative points", () => {
 			const line = new Line({ x: -1, y: 2 }, { x: 3, y: -4 });
 			assert.strictEqual(line.slope, -1.5);
 		});
-		it("Should give slope of given line having same y coordinate points", () => {
+
+		it("Should give slope of given line having y2 == y1 and x2 > x1", () => {
 			const line = new Line({ x: 1, y: 2 }, { x: 2, y: 2 });
 			assert.strictEqual(line.slope, 0);
 		});
-		it("Should give slope of given line having same x coordinate points", () => {
+
+		it("Should give slope of given line having y2 == y1 and x2 < x1", () => {
+			const line = new Line({ x: 2, y: 2 }, { x: 1, y: 2 });
+			assert.strictEqual(line.slope, -0);
+		});
+
+		it("Should give slope of given line having x2 == x1 and y2 > y1", () => {
 			const line = new Line({ x: 1, y: 2 }, { x: 1, y: 3 });
 			assert.strictEqual(line.slope, Infinity);
+		});
+
+		it("Should give slope of given line having x2 == x1 and y2 < y1", () => {
+			const line = new Line({ x: 1, y: 3 }, { x: 1, y: 2 });
+			assert.strictEqual(line.slope, -Infinity);
+		});
+
+		it("Should give NaN if both points are same", () => {
+			const line = new Line({ x: 1, y: 2 }, { x: 1, y: 2 });
+			assert.isNaN(line.slope);
 		});
 	});
 
@@ -78,26 +98,31 @@ describe("Line", () => {
 			const line2 = new Line({ x: 5, y: 6 }, { x: 7, y: 8 });
 			assert.ok(line1.isParallelTO(line2));
 		});
+
 		it("Should validate for two parallel lines having negative points", () => {
 			const line1 = new Line({ x: -1, y: -2 }, { x: -3, y: -4 });
 			const line2 = new Line({ x: -5, y: -6 }, { x: -7, y: -8 });
 			assert.ok(line1.isParallelTO(line2));
 		});
+
 		it("Should validate for two nonparallel lines having positive points", () => {
 			const line1 = new Line({ x: 1, y: 2 }, { x: 3, y: 4 });
 			const line2 = new Line({ x: 5, y: 9 }, { x: 7, y: 8 });
 			assert.notOk(line1.isParallelTO(line2));
 		});
+
 		it("Should validate for two nonparallel lines having negative points", () => {
 			const line1 = new Line({ x: -1, y: -2 }, { x: -3, y: -4 });
 			const line2 = new Line({ x: -5, y: 6 }, { x: 7, y: -8 });
 			assert.notOk(line1.isParallelTO(line2));
 		});
+
 		it("Should validate lines having same points", () => {
 			const line1 = new Line({ x: -1, y: -2 }, { x: -3, y: -4 });
 			const line2 = new Line({ x: -1, y: -2 }, { x: -3, y: -4 });
 			assert.notOk(line1.isParallelTO(line2));
 		});
+
 		it("should invalidate if a object is given that is not an instance of Line class", () => {
 			const line1 = new Line({ x: 1, y: 2 }, { x: 3, y: 4 });
 			const line2 = { start: { x: 1, y: 2 }, end: { x: 3, y: 4 } };
