117842eddc376863f64bf56dff7139acfc717eb6
Vadla Naveen Kumar <58026402+naveenkumarvadla@users.noreply.github.com>
2019-12-13 14:28:56 +0530

    Added isEquaTo function
    What changes you have made?
    1. Added sEqualTo function
    2. It will check whether two objects are equal or not based on
        a.instrance of objects
        b.starting points of objects
        c.ending points of objects
    
    Why this change is necessary?
    to check whether two objects are same or not
    
    What is the impact of the change?
    can check whether two objects are same or equal or not

--- src/line.js
+++ src/line.js
@@ -1,5 +1,11 @@
 "use strict";
 
+const arePointsEqual = function(point1, point2) {
+	const areXCoordinatesEqual = point1.x == point2.x;
+	const areYCoordinatesEqual = point1.y == point2.y;
+	return areXCoordinatesEqual && areYCoordinatesEqual;
+};
+
 class Line {
 	constructor(start, end) {
 		this.start = { ...start };
@@ -10,6 +16,15 @@ class Line {
 		const startingPoints = `(${this.start.x},${this.start.y})`;
 		const endingPoints = `(${this.end.x},${this.end.y})`;
 		return `${constructorName} ${startingPoints} ----- ${endingPoints}`;
+	}
+	isEqualTo(anotherLine) {
+		const isInstanceOfLine = anotherLine instanceof Line;
+		const areStartingPointsEqual = arePointsEqual(
+			this.start,
+			anotherLine.start
+		);
+		const areEndingPointsEqual = arePointsEqual(this.end, anotherLine.end);
+		return isInstanceOfLine && areStartingPointsEqual && areEndingPointsEqual;
 	}
 }
 
--- test/testLine.js
+++ test/testLine.js
@@ -3,12 +3,38 @@
 const assert = require("assert");
 const Line = require("../src/line");
 
-describe("Line", function() {
-	describe("toString", function() {
+describe("Line", () => {
+	describe("toString", () => {
 		it("Should give the string representation of given object ", function() {
 			const lineObj = new Line({ x: 1, y: 2 }, { x: 3, y: 4 });
 			const expected = `Line (1,2) ----- (3,4)`;
 			assert.strictEqual(lineObj.toString(), expected);
+		});
+	});
+
+	describe("isEqualTO", () => {
+		it("should validate if equal lines are given", () => {
+			const line1 = new Line({ x: 1, y: 2 }, { x: 3, y: 4 });
+			const line2 = new Line({ x: 1, y: 2 }, { x: 3, y: 4 });
+			assert.ok(line1.isEqualTo(line2));
+		});
+
+		it("should invalidate if lines of unequal start points are given", () => {
+			const line1 = new Line({ x: 1, y: 2 }, { x: 3, y: 4 });
+			const line2 = new Line({ x: 0, y: 0 }, { x: 3, y: 4 });
+			assert.ok(!line1.isEqualTo(line2));
+		});
+
+		it("should invalidate if lines of unequal end points are given", () => {
+			const line1 = new Line({ x: 1, y: 2 }, { x: 3, y: 4 });
+			const line2 = new Line({ x: 1, y: 2 }, { x: 0, y: 0 });
+			assert.ok(!line1.isEqualTo(line2));
+		});
+
+		it("should invalidate if a object is given that is not an instance of Line class", () => {
+			const line1 = new Line({ x: 1, y: 2 }, { x: 3, y: 4 });
+			const line2 = { start: { x: 1, y: 2 }, end: { x: 3, y: 4 } };
+			assert.ok(!line1.isEqualTo(line2));
 		});
 	});
 });
