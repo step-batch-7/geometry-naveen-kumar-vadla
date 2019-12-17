4279faffac653d0775781e0168dad26a60a90d72
Vadla Naveen Kumar <58026402+naveenkumarvadla@users.noreply.github.com>
2019-12-17 16:33:59 +0530

    Removed getSides function in rectangle.js
    What changes you have made?
    1. Removed getSides functtion in rectangle.js
    2. Modified Rectangle class constructor
    3. Modified area Property of Rectangle class
    4. Modified toString function of Rectangle class

--- src/rectangle.js
+++ src/rectangle.js
@@ -1,28 +1,22 @@
 "use strict";
 
 const Line = require("./line");
-
-const getSides = diagonal => {
-	const { start, end } = diagonal;
-	const AB = new Line(start, { x: end.x, y: start.y });
-	const DA = new Line(start, { x: start.x, y: end.y });
-	const BC = new Line(end, { x: end.x, y: start.y });
-	const CD = new Line(end, { x: start.x, y: end.y });
-	return { AB: AB, BC: BC, CD: CD, DA: DA };
-};
+const Point = require("./point");
 
 class Rectangle {
 	constructor(start, end) {
-		this.diagonal = new Line(start, end);
+		this.A = new Point(start.x, start.y);
+		this.C = new Point(end.x, end.y);
+		this.B = new Point(end.x, start.y);
+		this.D = new Point(start.x, end.y);
 	}
 
 	toString() {
-		return `[Rectangle (${this.diagonal.start.x},${this.diagonal.start.y}) to (${this.diagonal.end.x},${this.diagonal.end.y})]`;
+		return `[Rectangle (${this.A.x},${this.A.y}) to (${this.C.x},${this.C.y})]`;
 	}
 
 	get area() {
-		const { AB, BC } = getSides(this.diagonal);
-		return AB.length * BC.length;
+		return this.A.findDistanceTo(this.B) * this.B.findDistanceTo(this.C);
 	}
 }
 
