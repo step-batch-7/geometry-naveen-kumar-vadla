971980c48eff3e9ea4390e648273437e92cdc5cf
Vadla Naveen Kumar <58026402+naveenkumarvadla@users.noreply.github.com>
2019-12-17 17:45:13 +0530

    Modified this variables names in Rectangle class
    What changes you have made?
    1. Modified this variables names in Rectangle class
        this.A ==> this.vertexA
        this.B ==> this.vertexB
        this.C ==> this.vertexC
        this.D ==> this.vertexD

--- src/rectangle.js
+++ src/rectangle.js
@@ -5,22 +5,29 @@ const Point = require("./point");
 
 class Rectangle {
 	constructor(start, end) {
-		this.A = new Point(start.x, start.y);
-		this.C = new Point(end.x, end.y);
-		this.B = new Point(end.x, start.y);
-		this.D = new Point(start.x, end.y);
+		this.vertexA = new Point(start.x, start.y);
+		this.vertexC = new Point(end.x, end.y);
+		this.vertexB = new Point(end.x, start.y);
+		this.vertexD = new Point(start.x, end.y);
 	}
 
 	toString() {
-		return `[Rectangle (${this.A.x},${this.A.y}) to (${this.C.x},${this.C.y})]`;
+		return `[Rectangle (${this.vertexA.x},${this.vertexA.y}) to (${this.vertexC.x},${this.vertexC.y})]`;
 	}
 
 	get area() {
-		return this.A.findDistanceTo(this.B) * this.B.findDistanceTo(this.C);
+		return (
+			this.vertexA.findDistanceTo(this.vertexB) *
+			this.vertexB.findDistanceTo(this.vertexC)
+		);
 	}
 
 	get perimeter() {
-		return 2 * (this.A.findDistanceTo(this.B) + this.B.findDistanceTo(this.C));
+		return (
+			2 *
+			(this.vertexA.findDistanceTo(this.vertexB) +
+				this.vertexB.findDistanceTo(this.vertexC))
+		);
 	}
 }
 
